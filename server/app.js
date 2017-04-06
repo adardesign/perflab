0a08674bd0f5809a044860a5cb4b37bc1fa08274
Eliazer Braun <adardesign@gmail.com>
2017-04-06 18:23:58 -0400

    setting up dynamic server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
+var fs = require('fs') 
 
+app.set('view engine', 'ejs')
 
 app.set('port', (process.env.PORT || 5000));
-//app.use(express.static(__dirname + '/../client/'));
-app.use('/', express.static(__dirname + '/../client/'));
 
-app.listen(app.get('port'));
+app.use('/client', express.static(__dirname + '/../client/'));
 
+app.get(["/", "/:page"], (req, res) => {
+  res.render('../views/index.ejs', {
+      pageType:""
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log('listening')
+});
