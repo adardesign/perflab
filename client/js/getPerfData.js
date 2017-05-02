6f86fca7db840b2f7a19098d91e6342a191e013f
Eliazer Braun <adardesign@gmail.com>
2017-05-02 18:07:08 -0400

    bcu added

---
+++ client/js/getPerfData.js
@@ -0,0 +1,8 @@
+// http://deanhume.com/Home/PageSpeed
+// http://www.deanhume.com/Home/BlogPost/PageSpeed/86
+//https://console.developers.google.com/apis/api/pagespeedonline.googleapis.com/overview?project=perflab-163717&duration=PT12H
+// https://developers.google.com/speed/docs/insights/v2/getting-started?hl=en_US
+// https://developers.google.com/apis-explorer/#p/pagespeedonline/v2/pagespeedonline.pagespeedapi.runpagespeed?url=https%253A%252F%252Fwww.adorama.com%252F&screenshot=true&strategy=desktop&_h=1&
+// https://github.com/mkalygin/fcm-demo
+// view-source:https://fcm-demo-mtbqzgkaoz.now.sh/
+fetch("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=https://developers.google.com/speed/pagespeed/insights/&strategy=mobile&key=AIzaSyAtAMaioo60eXpUJNn3BZTGiOZ2HPPC7BY")
---
+++ client/js/pushNotifications.js
@@ -0,0 +1,3 @@
+// https://firebase.google.com/docs/cloud-messaging/js/send-multiple#about_topic_messaging
+// https://framework.realtime.co/demo/web-push/
+// https://github.com/realtime-framework/WebPushNotifications
