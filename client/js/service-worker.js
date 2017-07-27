9e61d7898b77b76a2d9d3cf28fcea61d2840f0fc
Eliazer Braun <adardesign@gmail.com>
2017-07-27 16:00:11 -0400

    appshell demo 1

--- client/js/service-worker.js
+++ client/js/service-worker.js
@@ -19,7 +19,7 @@
 
 // self.importScripts('/client/js/idb-keyval-min.js', "/client/js/sw-utils.js");
 
-
+console.log("SW");
 
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime-v1';
@@ -29,8 +29,8 @@ const PRECACHE_URLS = [
     // 'index.html',
     // './', // Alias for index.html
     //'pageWitoutSW.html',
-    "/appShell",
-    "/client/js/appShell.js"
+    "/appshell.html",
+    "/client/images/shell-home-page-placeholder.jpg"
 ];
 
 const appShell = "/appShell";
@@ -77,16 +77,18 @@ self.addEventListener('fetch', function(event) {
     // if(!isRequestCacheable(event.request.url)){}
 
     openPreCache = caches.open(PRECACHE);
+    
     if (event.request.mode === "navigate" && event.request.headers.get('Accept').includes("text/html")) {
         event.respondWith(
                 openPreCache.then(function(cache) {
-                    return cache.match("/appShell").then(function(response) {
+                    return cache.match("/appshell.html").then(function(response) {
                         // respond from the cache, or the network
                         return response || false;
                     });
                 })
             ) //appShell
     } else {
+        return;
         event.respondWith(
             openPreCache.then(function(cache) {
                 return cache.match(event.request).then(function(response) {
@@ -106,85 +108,3 @@ self.addEventListener('fetch', function(event) {
         )
     }
 });
-
-//
-// messaging
-//         self.addEventListener('message', function(event) {
-//           console.log('Handling message event:', event);
-//           var openTheCachePromise = caches.open(RUNTIME).then(function(cache) {
-//             switch (event.data.command) {
-//               // This command returns a list of the URLs corresponding to the Request objects
-//               // that serve as keys for the current cache.
-//               case 'keys':
-//                 return cache.keys().then(function(requests) {
-//                   var urls = requests.map(function(request) {
-//                     return request.url;
-//                   });
-//         
-//                   return urls.sort();
-//                 }).then(function(urls) {
-//                   // event.ports[0] corresponds to the MessagePort that was transferred as part of the controlled page's
-//                   // call to controller.postMessage(). Therefore, event.ports[0].postMessage() will trigger the onmessage
-//                   // handler from the controlled page.
-//                   // It's up to you how to structure the messages that you send back; this is just one example.
-//                   sendMessageToAllClients({dataType:"keys", urls: urls? urls : "fail fetching"});
-//                 });
-//               break;
-//               
-//               // This command adds a new request/response pair to the cache.
-//               case 'add':
-//                 // If event.data.url isn't a valid URL, new Request() will throw a TypeError which will be handled
-//                 // by the outer .catch().
-//                 // Hardcode {mode: 'no-cors} since the default for new Requests constructed from strings is to require
-//                 // CORS, and we don't have any way of knowing whether an arbitrary URL that a user entered supports CORS.
-//                 var url = event.data.url;
-//                 var request = new Request(url, {mode: 'no-cors'});
-//                 return fetch(request).then(function(response) {
-//                   return cache.put(url, response);
-//                 }).then(function() {
-//                   sendMessageToAllClients({dataType:"add", url:url, status: "success"});
-//                 });
-//                 break;
-//               
-//               // This command removes a request/response pair from the cache (assuming it exists).
-//               case 'delete':
-//                 var url = event.data.url;
-//                 sendMessageToAllClients("attempting delete cache");
-//                 return cache.delete(url).then(function(success) {
-//                     sendMessageToAllClients({dataType:"delete", url:url, status: success ? "success" : "fail"});
-//                 });
-//         
-//                 break;
-//         
-//               case 'stale-storage':
-//                console.log("stale-storage");
-//         
-//               break;
-//         
-//               case 'set-cache-version':
-//                idbKeyval.set('resource-cache-version', event.data.resource);
-//         
-//               default:
-//                 // This will be handled by the outer .catch().
-//                 throw Error('Unknown command: ' + event.data.command);
-//             }
-//           }).catch(function(error) {
-//             // If the promise rejects, handle it by returning a standardized error message to the controlled page.
-//             console.error('Message handling failed:', error);
-//         
-//           //  event.ports[0].postMessage({
-//           //    error: error.toString()
-//           //  });
-//           });
-//
-//             // Beginning in Chrome 51, event is an ExtendableMessageEvent, which supports
-//             // the waitUntil() method for extending the lifetime of the event handler
-//             // until the promise is resolved.
-//             if ('waitUntil' in event) {
-//                event.waitUntil(openTheCachePromise);
-//             }
-//
-//             // Without support for waitUntil(), there's a chance that if the promise chain
-//             // takes "too long" to execute, the service worker might be automatically
-//             // stopped before it's complete.
-//            );
--- server/app.js
+++ server/app.js
@@ -1,18 +1,16 @@
-const cacheVersion = "6"; // not used when taken from firebase.
 const express = require('express');
 var app = express();
-var fs = require('fs') 
-var request = require('request');
+var fs = require('fs')
 
 app.set('view engine', 'ejs')
 
 app.set('port', (process.env.PORT || 5000));
 
 // caching..
-app.use( (req, res, next) => {
+app.use((req, res, next) => {
     //if (req.url.match(/\.(css|js|img|font|html)\/.+/)) {
-        // res.setHeader('Cache-Control', 'public, max-age=3600')
-        // res.setHeader('test-eb', 'OK')
+    // res.setHeader('Cache-Control', 'public, max-age=3600')
+    // res.setHeader('test-eb', 'OK')
     //}
     next();
 });
@@ -24,45 +22,34 @@ app.get('/favicon.ico', function(req, res) {
 app.use('/client', express.static(__dirname + '/../client/'));
 
 // service-worker
-app.use('/service-worker.js', express.static(__dirname+ '/../client/js/service-worker.js'));
-app.use('/manifest.json', express.static(__dirname+ '/../client/manifest.json'));
+app.use('/service-worker.js', express.static(__dirname + '/../client/js/service-worker.js'));
+app.use('/manifest.json', express.static(__dirname + '/../client/manifest.json'));
 
-app.get("/appShell", (req, res) => {
-      res.render('../views/appShell.ejs');
-});
 
-app.get(["/", "/:page", "/:catagory/:page"], (req, res) => {
-  const url = 'https://perflab-163717.firebaseio.com/.json';
-  let pageType = req.params && req.params.catagory ? req.params.catagory : "top level";
-    request(url, (error, response, body)=> {
-      if (!error && response.statusCode === 200) {
-          responseJson = JSON.parse(body);
-          cacheVersion = responseJson.cacheVersion  || cacheVersion;
-          res.render('../views/index.ejs', {
-            request:req,
-            pageType:pageType,
-            cacheVersion:cacheVersion,
-            currentUrl:req.url
-        });
-      } else {
-          res.render('../views/index.ejs', {
-          pageType:"Home",
-          request:req,
-          cacheVersion:cacheVersion,
-          currentUrl:req.url
-        });
-      }
-    });
+
+app.get("/", (req, res) => {
+    var options = {
+        root: __dirname + '/../client/'
+    };
+    if (req.query.fragment) {
+        setTimeout((function() {
+            res.sendFile("homepage.content.html", options);
+        }), 800);
+
+    } else {
+        res.sendFile("homepage.html", options);
+    }
 });
 
 
-
-app.get("/api/getAsyncData", (req, res) => {
-  res.json({
-    cacheVersion:cacheVersion
-  });
+app.get("/appshell.html", (req, res) => {
+    var options = {
+        root: __dirname + '/../client/'
+    };
+    res.sendFile("appshell.html", options);
 });
+
 
 app.listen(app.get('port'), () => {
-  console.log('listening')
+    console.log('listening')
 });
